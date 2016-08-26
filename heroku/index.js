712cb2fedff95e669aaf0844fcbe4e7cd05ce090
Unni Nambiar <unni.nambiar@gmail.com>
2016-08-26 22:25:12 +0530

    Changed Bot name and avatarUrl
    to UnniBot and unnibot.jpg

--- heroku/index.js
+++ heroku/index.js
@@ -31,8 +31,8 @@ class BetterSmoochApiBot extends SmoochApiBot {
     }
 }
 
-const name = 'SmoochBot';
-const avatarUrl = 'https://s.gravatar.com/avatar/f91b04087e0125153623a3778e819c0a?s=80';
+const name = 'UnniBot';
+const avatarUrl = 'https://raw.githubusercontent.com/unninambiar/UnniBot/master/img/unnibot.jpg';
 const store = new SmoochApiStore({
     jwt
 });
