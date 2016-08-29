4c20f5c55e78de009fab828c5d2d1be285af5ea7
Unni Nambiar <unni.nambiar@gmail.com>
2016-08-30 00:35:36 +0530

    Debugging regex

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    if (!_.find(scriptRules, function(rule) { return _.isEqual('Hello.*', upperText); })) {
+                    if (!_.find(scriptRules, function(rule) { return _.isEqual('HELLO.*', upperText); })) {
                         return bot.say(`I haven\'t learnt how to respond to that yet.  Would you like to teach me?  %[Teach UnniBot](postback:teach)`)
                             .then(() => 'speak');
                     }
